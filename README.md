de3c6af652b014c7f50cd7b3abad3edbc7f5432d
ThomasBechet <thomas.bechet.pro@gmail.com>
2021-05-24 23:46:28 +0200

    Organizing the project for final commit

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # BinPacking1D
 
+# Structure du projet
+
+- */data* : contient les jeux de données utiles aux tests.
+- */docs* : contient le sujet du projet ainsi que le rapport.
+- */lib* : contient la bibliothèque lp_solve, utile à la question 1 du projet. 
+- */scripts* : contient les scripts qui ont permis de créer les graphiques.
+- */src* : contient les sources du projet
+
 # Installation
 
-Afin de permettre le bon fonctionnement du package "lv_solve", il est necessaire d'ajouter le chemin "/lib/lp_solve" aux variables d'environnement.
+- Afin de permettre le bon fonctionnement du package "lv_solve", il est necessaire d'ajouter le chemin "/lib/lp_solve" aux variables d'environnement. Le package est déjà présent dans le projet dans "/lib/lp_solve". Il suffit donc de rajouter le chemin du dossier "lp_solve" dans les variables d'environement (voir l'image).
+- Il faut ensuite ajouter "lp_solve.jar" aux dépendances du projet. Selon l'IDE, l'installation peut varier.
 
-Le package est déjà présent dans le projet dans "/lib/lp_solve". Il suffit donc de rajouter le chemin du dossier "lp_solve" dans les variables d'environement.
-
-**Exemple**
+**Exemple de variable d'environnement**
 
 ![image](docs/exemple.png)
+
+# Lancement de l'application
+
+Le projet peut être ouvert avec n'importe quel IDE (Intellij est privilégié). Après avoir installé les dépendances, il faut ajouter le répertoire source pour être compilé par votre IDE. Des exemples d'exécution du projet ont été laissés dans la classe Main du projet. Il suffit de dé-commenter les parties à exécuter.
--- src/Main.java
+++ src/Main.java
@@ -6,49 +6,57 @@ public class Main {
 
         /* LP SOLVE DEMO */
 
-        DataSet dataSet = new DataSet("binpack1d_simple.txt");
-        Solution lpSolution = SolutionBuilder.findBestSolutionLpSolve(dataSet);
-        for (Bin bin : lpSolution.getBins()) {
-            for (Item i : bin.getItems()) {
-                System.out.print(i.getValue() + " ");
-            }
-            System.out.println();
-        }
+//        DataSet dataSet = new DataSet("binpack1d_simple.txt");
+//        Solution lpSolution = SolutionBuilder.findBestSolutionLpSolve(dataSet);
+//        for (Bin bin : lpSolution.getBins()) {
+//            for (Item i : bin.getItems()) {
+//                System.out.print(i.getValue() + " ");
+//            }
+//            System.out.println();
+//        }
 
-//        (new PrintWriter("record.txt")).close();
-//        PrintWriter recorder = new PrintWriter("record.txt");
+        /* First fit DEMO */
 
-//        DataSet dataSet = new DataSet("binpack1d_05.txt");
+//        DataSet dataSet = new DataSet("binpack1d_00.txt");
 //        Random rng = new Random(12345);
-//        System.out.println(dataSet.getLowerBound() + " " + dataSet.getUpperBound());
-//        Solution initialSolution = SolutionBuilder.oneItemPerBin(dataSet);
-//        int firstFitSortedFitness = SolutionBuilder.firstFitSorted(dataSet).fitness();
-//        int firstFitFitness = SolutionBuilder.firstFitRandom(dataSet, rng).fitness();
-//        System.out.println(firstFitSortedFitness);
-//        System.out.println(firstFitFitness);
+//        System.out.println("Lower bound: " + dataSet.getLowerBound() + " Upper bound: " + dataSet.getUpperBound());
+//        System.out.println("First fit random => " + SolutionBuilder.firstFitRandom(dataSet, rng));
+//        System.out.println("First fit sorted => " + SolutionBuilder.firstFitSorted(dataSet));
+
 //        recorder.println("fitness: " + firstFitSortedFitness + " " + firstFitFitness);
 //        recorder.println(300 * 5 + " " + 1500);
 
         /* Recuit Simulé DEMO */
 
+//        DataSet dataSet = new DataSet("binpack1d_05.txt");
+//        Random rng = new Random(12345);
+//        Solution initialSolution = SolutionBuilder.oneItemPerBin(dataSet);
 //        SolutionBuilder.RecuitSimuleParameters RSParameters = new SolutionBuilder.RecuitSimuleParameters();
 //        RSParameters.initialTemperature        = 10000.0f;
 //        RSParameters.temperatureDecreaseFactor = 0.99f;
 //        RSParameters.temperatureChangeCount    = 300;
 //        RSParameters.iterationPerTemperature   = 5;
 //        RSParameters.neighbourCount            = 100;
-//        RSParameters.recorder                  = recorder; // peut être mis à null pour ignorer l'enregistrement
-//        System.out.println("Recuit simulé: " + SolutionBuilder.findBestSolutionRecuitSimule(initialSolution, rng, RSParameters).fitness());
+//        RSParameters.recorder                  = null; // peut être mis à null pour ignorer l'enregistrement
+//        System.out.println("Recuit simulé => " + SolutionBuilder.findBestSolutionRecuitSimule(initialSolution, rng, RSParameters));
 
         /* Recherche Tabou DEMO */
 
+//        DataSet dataSet = new DataSet("binpack1d_05.txt");
+//        Random rng = new Random(12345);
+//        Solution initialSolution = SolutionBuilder.oneItemPerBin(dataSet);
 //        SolutionBuilder.TabuSearchParameters TSParameters = new SolutionBuilder.TabuSearchParameters();
 //        TSParameters.iterationCount = 1500;
 //        TSParameters.queueLength    = 5;
 //        TSParameters.neighbourCount = 100;
-//        TSParameters.recorder = recorder; // peut être mis à null pour ignorer l'enregistrement
-//        System.out.println("Tabu search: " + SolutionBuilder.findBestSolutionTabuSearch(initialSolution, rng, TSParameters).fitness());
+//        TSParameters.recorder = null; // peut être mis à null pour ignorer l'enregistrement
+//        System.out.println("Tabu search => " + SolutionBuilder.findBestSolutionTabuSearch(initialSolution, rng, TSParameters));
 
+        /* recorder (pour les scripts python) */
+
+//        (new PrintWriter("record.txt")).close();
+//        PrintWriter recorder = new PrintWriter("record.txt");
+        // Ajouter algo avec recorder activé dans les paramètres
 //        recorder.flush();
     }
 }
